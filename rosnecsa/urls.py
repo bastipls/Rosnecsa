40742d2c490a6c0b6281b8612d9ebdf361498657
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2018-12-12 21:04:56 -0300

    Agrego view de registro y edito htmls

--- rosnecsa/urls.py
+++ rosnecsa/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.login_view, name='login'),
-   path('login/registro_inicial/',views.registro_inicial,name='registro_inicial'),
-   path('logout/',views.logout_view, name="logout"),
-   path('crear_folio/',views.crear_folio, name='crear_folio'),
-
+    path('', views.login_view, name='login'),
+    path('logout/',views.logout_view, name="logout"),
+    path('login/registro_inicial/',views.registro_inicial,name='registro_inicial'),
+    path('listar_clientes/',views.listar_clientes, name='listar_clientes'),
+    path('crear_folio/',views.crear_folio, name='crear_folio'),
 ]
--- rosnecsa/views.py
+++ rosnecsa/views.py
@@ -10,25 +10,48 @@ def login_view(request):
     context = {}
     if request.method == 'POST':
         username = request.POST.get('txtuser',True)
-        password = request.POST['txtpass']
+        password = request.POST['txtpass']#Y29waW9u
         user = authenticate(request,username=username ,password = password)
         if user: 
             login(request,user)
-            return HttpResponseRedirect(reverse('crear_folio'))
+            return HttpResponseRedirect(reverse('listar_clientes'))
         else:
             context["error"] = "Credenciales Incorrectas"
             return render(request, 'rosnecsa/login.html',context)    
     return render(request , 'rosnecsa/login.html',context)
+
 def logout_view(request):
     if request.method == 'POST':
-        logout(request)
+        logout(request)#Y29waW9u
     return redirect('login')
     
 def registro_inicial(request):
-
     context = {}
+    if not request.user.is_anonymous:
+        return redirect('login')
+    else:
+        if request.method == 'POST':
+                rutTecnico = request.POST['txtrut']
+                nombreTecnico = request.POST['txtname']
+                apellidoTecnico = request.POST['txtlastname']
+                emailTecnico = request.POST['txtemail']#Y29waW9u
+                contraseñaTecnico = request.POST['txtpass']
 
-    return render(request,'rosnecsa/registro_inicial.html',context)
+                user = User.objects.create_user(username=emailTecnico,email=emailTecnico,password=contraseñaTecnico,first_name=nombreTecnico,last_name=apellidoTecnico)
+                user.save()  
+                atributos =  Tecnico(rut_tecnico = rutTecnico,
+                                    email_tecnico = emailTecnico,
+                                    nombre_tecnico = nombreTecnico,
+                                    apellido_tecnico = apellidoTecnico)
+                atributos.save()
+                context["success"] = "Cuenta creada con exito"
+                return render(request,'rosnecsa/login.html',context)
+    
+
+        return render(request,'rosnecsa/registro_inicial.html',context)
+def listar_clientes (request):
+    context = {}
+    return render(request,'rosnecsa/listar_clientes.html')
 
 def crear_folio(request):
 
