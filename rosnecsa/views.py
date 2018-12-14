560dfe83bb5e333bbaa5d0f2494b766efed660ce
anais <ana.acevedo@alumnos.duoc.cl>
2018-12-13 21:21:07 -0300

    arreglo buggg

--- rosnecsa/views.py
+++ rosnecsa/views.py
@@ -23,7 +23,7 @@ def login_view(request):
     return render(request , 'rosnecsa/login.html',context)
 
 def logout_view(request):
-    if request.user.is_authenticated:
+    if not request.user.is_authenticated:
         return redirect('login')
     else:
         if request.method == 'POST':
