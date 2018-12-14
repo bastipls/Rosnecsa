4d8a3a2ee1d2c204e5b988e5bb6405fe89baaf9f
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2018-12-13 22:03:14 -0300

    arreglo ubg

--- rosnecsa/views.py
+++ rosnecsa/views.py
@@ -120,7 +120,3 @@ def asignar_cliente (request,pk):
         form = TecnicoForm(instance=tecnico)
     context= {'form':form}
     return render(request,'rosnecsa/asignar_cliente.html',context)         
-
-def listar_folios(request):
-    context = {}
-    return render(request,'rosnecsa/listar_folios.html',context)
